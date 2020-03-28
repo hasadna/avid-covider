8ecf2f531aaee5046b0f9d917b1b1a26e61c8008
Adam Kariv <adam.kariv@gmail.com>
2020-03-28 21:23:25 +0300

    move version to correct place

--- src/app/intro-page/intro-page.component.ts
+++ src/app/intro-page/intro-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output, Inject, LOCALE_ID, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { NotificationService } from '../notification.service';
+import { VERSION } from '../constants';
 
 @Component({
   selector: 'app-intro-page',
@@ -11,12 +12,14 @@ export class IntroPageComponent implements OnInit, AfterViewInit {
   @Output() info = new EventEmitter<void>();
   @Output() chat = new EventEmitter<void>();
 
+  version = VERSION;
+
   @ViewChild('notificationTitle') notificationTitle: ElementRef;
   @ViewChild('notificationBody') notificationBody: ElementRef;
   @ViewChild('notificationAction') notificationAction: ElementRef;
 
   constructor(private notifications: NotificationService,
-              @Inject(LOCALE_ID) public locale) { }
+              @Inject(LOCALE_ID) public locale) {}
 
   ngOnInit() {
   }
