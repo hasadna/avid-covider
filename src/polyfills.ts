bc248f83734726a63a6a5d6436ac3748175f9ba8
Adam Kariv <adam.kariv@gmail.com>
2020-03-31 10:50:55 +0300

    Add polyfill for object.entries error

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -35,6 +35,9 @@ import 'core-js/es6/map';
 import 'core-js/es6/weak-map';
 import 'core-js/es6/set';
 
+/** Object.entries */
+import 'core-js/es7/object';
+
 /**
  * If the application will be indexed by Google Search, the following is required.
  * Googlebot uses a renderer based on Chrome 41.
