5af313dcf91d9490bbb519a2b541602f072d76e0
Adam Kariv <adam.kariv@gmail.com>
2020-03-31 13:15:15 +0300

    Add smooth-scroll polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -91,3 +91,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import smoothscroll from 'smoothscroll-polyfill';
+smoothscroll.polyfill();
