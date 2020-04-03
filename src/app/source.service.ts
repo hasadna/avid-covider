28eacaf483c18f3f1d78128d0fcccaf5b1199866
Adam Kariv <adam.kariv@gmail.com>
2020-04-03 15:33:11 +0300

    Accept also utm query param as source

--- src/app/source.service.ts
+++ src/app/source.service.ts
@@ -10,7 +10,7 @@ export class SourceService {
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router) {
     this.activatedRoute.queryParamMap.subscribe((queryParamMap) => {
-      this.source = queryParamMap.get('source') || this.source;
+      this.source = queryParamMap.get('source') || queryParamMap.get('utm') || this.source;
       this.router.navigate([], { queryParams: {source: null} });
     });
   }
