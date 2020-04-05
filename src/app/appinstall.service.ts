27560dde0c4689f2f162688ea14ea5f7a604c991
Adam Kariv <adam.kariv@gmail.com>
2020-04-05 18:26:35 +0300

    App install badge for desktop as well

--- src/app/appinstall.service.ts
+++ src/app/appinstall.service.ts
@@ -7,21 +7,37 @@ import { fromEvent } from 'rxjs';
 export class AppinstallService {
 
   installPrompt: any;
+  platforms = ['play', 'itunes'];
+  prompts = [
+    this.openUrl('https://play.google.com/store/apps/details?id=il.ac.weizmann.corona'),
+    this.openUrl('https://itunes.com'),
+  ];
 
   constructor() {
     fromEvent(window, 'beforeinstallprompt').subscribe(($event) => {
       $event.preventDefault();
       this.installPrompt = $event;
+      this.platforms = this.installPrompt.platforms || [];
+      this.prompts = [];
+      for (const platform of this.platforms) {
+        this.prompts.push(() => this.installPrompt.prompt());
+      }
     });
   }
 
+  openUrl(url) {
+    return () => {
+      window.open(url, '_blank');
+      return Promise.resolve(true);
+    };
+  }
+
   showButton() {
-    return this.androidAvailable() || this.iphoneAvailable() || true;
+    return this.androidAvailable() || this.iphoneAvailable();
   }
 
   platformAvailable(platform) {
-    return this.installPrompt && this.installPrompt.platforms &&
-      Array.isArray(this.installPrompt.platforms) && this.installPrompt.platforms.indexOf(platform) >= 0;
+    return this.platforms && Array.isArray(this.platforms) && this.platforms.indexOf(platform) >= 0;
   }
 
   androidAvailable() {
@@ -32,13 +48,12 @@ export class AppinstallService {
     return this.platformAvailable('itunes');
   }
 
-  prompt() {
+  prompt(platform) {
     if (this.showButton()) {
-      this.installPrompt.prompt().then(
+      this.prompts[this.platforms.indexOf(platform)]().then(
         (res) => { console.log('Install Prompt', res); },
         (err) => { console.log('Install Prompt error', err); }
       );
     }
   }
-
 }
