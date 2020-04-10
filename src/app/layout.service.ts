549be86d4b1df2a99ef8d430ce05180155c0ee18
Adam Kariv <adam.kariv@gmail.com>
2020-04-10 17:22:55 +0300

    missing service

---
+++ src/app/layout.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { fromEvent } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LayoutService {
+  nativeElement: HTMLElement;
+  mobile: boolean;
+  desktop: boolean;
+  layout: string;
+
+  constructor() {
+    fromEvent(window, 'resize').subscribe(($event) => {
+      this.updateView();
+    });
+  }
+
+  updateView(nativeElement?: HTMLElement) {
+    if (nativeElement) {
+      this.nativeElement = nativeElement;
+    }
+    if (this.nativeElement) {
+      this.mobile = this.nativeElement.offsetWidth < 600;
+      this.desktop = this.nativeElement.offsetWidth >= 600;
+      this.layout = this.mobile ? 'mobile' : 'desktop';
+    }
+  }
+
+}
