6d55cba0ee7e6c17b9be023d0737a4fd19927814
Adam Kariv <adam.kariv@gmail.com>
2020-04-14 17:40:20 +0300

    Restore source=map

--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -18,18 +18,17 @@ export class MapService {
   zoom = 11;
 
   constructor(private source: SourceService) {
-  }
-
-  init() {
-    if (this._init) { return; }
-    this._init = true;
-
     this.source.sourceStream.pipe(first()).subscribe((_source) => {
       if (_source === 'map') {
         this.overlayOpen = true;
         this.mapVisible = true;
       }
     });
+  }
+
+  init() {
+    if (this._init) { return; }
+    this._init = true;
     (<any>mapboxgl).accessToken = 'pk.eyJ1Ijoid2lvcyIsImEiOiJjazh4ZXZ6Z24wejdtM3JvN2F1MHdlc2Z4In0.vz7knGcRWWGE4LGOLx8c7g';
     mapboxgl.setRTLTextPlugin(
       'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
