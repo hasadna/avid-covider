43fc436012e894f353fb9eabe86f47d97ccac9fa
yuvalbl <yuval.bar.levi@gmail.com>
2020-05-02 20:19:16 +0300

    basic random flow

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,24 +1,119 @@
-import { AppPage } from './app.po';
+import { AppPage, INextAnswer, AnswerType } from './app.po';
+import { browser, ElementFinder, ElementArrayFinder, protractor } from 'protractor';
 
+const MAX_ANSWERS_PER_REPORT = 30;
 const STR = {
   ben: 'בן',
   bat: 'בת',
+  reportSent: 'זהו, סיימנו',
 };
-
+const log = (msg, arg: any = '') => console.log(`[app.e2e-spec] ${msg}`, arg);
+/*******************************
+ * Concepts:
+ * There are 2 kinds of tests
+ * Blind test - will choose answers randomally
+ * Case test - for specified questions - answers specified answers. for others questions - choose answers randomally
+ */
 describe('test chatbot scenarion 1 - new user', () => {
   let page: AppPage;
+  const answers = [];
 
   beforeEach(() => {
+    // browser.executeScript('window.localStorage.clear();');
     page = new AppPage();
   });
 
-  it('should simulate a click for a new user', () => {
+  it('[Case] should simulate a "ben" click for a new user', () => {
     page.navigateTo();
 
     // click on "ben"
     page.getMessageOptions().first().click();
-    const lastMessage =  page.getMessagesTo().last().getText();
+    const lastMessage = page.getMessagesTo().last().getText();
     expect(lastMessage).toContain(STR.ben);
     expect(lastMessage).not.toContain(STR.bat);
   });
+
+  fit('[Blind] should simulate a "ben" click for a new user', async() => {
+    page.navigateTo();
+    let activeQuestionText;
+    let activeAnswerElement;
+    let reportSent = false;
+    for (let i = 0; i < MAX_ANSWERS_PER_REPORT && !reportSent; i++) {
+      browser.sleep(3000);
+      activeQuestionText = await page.getActiveQuestionText();
+      log('Active quesion:', activeQuestionText.toString().split('').reverse().join(''));
+      activeAnswerElement = await page.getNextAnswerElement();
+      await answerRandomally(activeAnswerElement);
+      // reportSent = (lastMessageText).includes(STR.reportSent);
+    }
+  });
 });
+
+// interaction functions
+async function answerRandomally(answer: INextAnswer) {
+  switch (answer.type) {
+    case AnswerType.InputText: {
+      const input = 'yada yada'
+      answer.input.sendKeys(input);
+      await confirmAnswer(answer.confirmElement);
+      log(`Answer using input-text: ${input}`);
+      break;
+    }
+    case AnswerType.InputNumber: {
+      const min = await answer.input.getAttribute('min');
+      const max = await answer.input.getAttribute('max');
+      const input = getRandomInt(parseInt(max, 10), parseInt(min, 10));
+      answer.input.sendKeys(input);
+      await confirmAnswer(answer.confirmElement);
+      log(`Answer using input-number: ${input}`);
+      break;
+    }
+    case AnswerType.InputDate: {
+      const input = (new Date()).toISOString();
+      answer.input.sendKeys(input);
+      await confirmAnswer(answer.confirmElement);
+      log(`Answer using input-date: ${input}`);
+      break;
+    }
+    case AnswerType.OptionsSingle: {
+      await selectRandomOptionSingle(answer.options);
+      break;
+    }
+    case AnswerType.OptionsMulti: {
+      await selectRandomOptionMulti(answer.options, answer.confirmElement);
+      break;
+    }
+  }
+}
+async function selectRandomOptionSingle(options: ElementArrayFinder) {
+  const lastOptionIndex = (await options.count()) - 1;
+  const clickBtnIndex = getRandomInt(lastOptionIndex);
+  log(`Answer using single-select option - click button index: ${clickBtnIndex}`);
+  options.get(clickBtnIndex).click();
+}
+
+async function selectRandomOptionMulti(options: ElementArrayFinder, confirmElement: ElementFinder) {
+  const lastOptionIndex = (await options.count()) - 1;
+  let clickBtnIndex = getRandomInt(lastOptionIndex);
+    // multi-select option
+    const howManyToSelect = 1; // remove
+    for (let index = 0; index < howManyToSelect; index++) {
+    // click randomally (same option may be clicked multiple times)
+    clickBtnIndex = getRandomInt(lastOptionIndex);
+    log(`Answer using multi-select option - click button index: ${clickBtnIndex} (last index is ${lastOptionIndex})`);
+    options.get(1).click(); // remove
+    confirmAnswer(confirmElement);
+  }
+}
+
+async function confirmAnswer(confirmElement: ElementFinder) {
+  const EC = protractor.ExpectedConditions;
+  browser.wait(EC.presenceOf(confirmElement), 5000);
+  confirmElement.click();
+}
+
+function getRandomInt(max, min = 0) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,4 +1,21 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder, ElementArrayFinder } from 'protractor';
+
+export enum AnswerType {
+  InputNumber = 'InputNumber',
+  InputText = 'InputText',
+  InputDate = 'InputDate',
+  OptionsSingle = 'OptionsSingle',
+  OptionsMulti = 'OptionsMulti',
+}
+
+export interface INextAnswer {
+  type: AnswerType;
+  input?: ElementFinder; // for type Input
+  options?: ElementArrayFinder; // for type Options
+  confirmElement?: ElementFinder; // Input OR Options(multi)
+}
+
+const log = (msg, arg: any = '') => console.log(`[App Page] ${msg}`, arg);
 
 export class AppPage {
   navigateTo() {
@@ -11,5 +28,81 @@ export class AppPage {
 
   getMessagesTo() {
     return element.all(by.css('htl-message-to .speech-bubble span'));
+  }
+
+  // == input ==
+  getHtlInput() {
+    return element(by.css('htl-input input'));
+  }
+
+  getHtlInputConfrim() {
+    return element(by.css('htl-input button'));
+  }
+
+  // == options ==
+  getActiveHtlSingleOptions() {
+    return element.all(by.css('htl-messages htl-message-options:last-child .options button'));
+  }
+
+  getActiveHtlMultiOptions(): ElementArrayFinder {
+    // get all options other than last ('other' is confirm button)
+    return element.all(by.css('htl-messages htl-message-multi-options:last-child .options button:not(.other)'));
+    // return allButtons.filter((elem, index) => elem !== allButtons.last());
+  }
+
+  getActiveHtlMultiOptionsConfirm(): ElementFinder {
+    // get last option ('other' is confirm button)
+    return element(by.css('htl-messages htl-message-multi-options:last-child .options button.other'));
+  }
+
+  // due to layout structure, this function may return 1-3 messages, depend on the displayed elements
+  // (To be specific: 'nth-last-child' will take the last 3 items, and will filter those who are not 'htl-message-to')
+  // async getLastMessagesText(): Promise<string> {
+  //   return element.all(by.css(`htl-message-to:nth-last-child(-n+3) .speech-bubble span`)).getText();
+  // }
+  async getActiveQuestionText(): Promise<string> {
+    return element.all(by.css('htl-message-to')).last().element(by.css('.speech-bubble span')).getText();
+  }
+
+  // get the next element will be used for answer
+  async getNextAnswerElement(): Promise<INextAnswer> {
+    const htlInput = await this.getHtlInput();
+    const isHtlInputPresent = await htlInput.isPresent();
+    const result: INextAnswer = { type: AnswerType.InputText };
+    // is hatool input active
+
+    if (isHtlInputPresent) {
+      // using htl input - return it
+      const inputType = await htlInput.getAttribute('type');
+      switch (inputType) {
+        case 'date': {
+          result.type = AnswerType.InputDate;
+          break;
+        }
+        case 'number': {
+          result.type = AnswerType.InputNumber;
+          break;
+        }
+        default: {
+          result.type = AnswerType.InputText;
+          break;
+        }
+      }
+      log(`Next answer element is using Hatool Input [${result.type}]`);
+      result.input = htlInput;
+      result.confirmElement = this.getHtlInputConfrim();
+    } else {
+      const htlSingleSelectOption = this.getActiveHtlSingleOptions();
+      // is active hatool options are of type single
+      const isSingle = await htlSingleSelectOption.isPresent();
+      log(`Next answer element is using Hatool Options [${isSingle ? 'single' : 'multi'}]`);
+
+      result.type = isSingle ? AnswerType.OptionsSingle : AnswerType.OptionsMulti;
+      result.options = isSingle ? htlSingleSelectOption : this.getActiveHtlMultiOptions();
+      if (!isSingle) {
+        result.confirmElement = this.getActiveHtlMultiOptionsConfirm();
+      }
+    }
+    return result;
   }
 }
